14e45155898a0d0179808676cef828ff05dbb141
benturnerrocks <turnerb2@carleton.edu>
2022-06-02 12:16:47 -0500

    slight wrangling addition

--- wrangling.r
+++ wrangling.r
@@ -4,6 +4,9 @@ library(readr)
 masterTitles <- read_csv("movie_database.csv")
 masterTitles <- masterTitles %>%
   group_by(title) %>% 
-  dplyr::mutate(streaming_service = ifelse(n()>1,paste(streaming_service, collapse = ", "),streaming_service))
+  dplyr::mutate(streaming_service = ifelse(n()>1,paste(streaming_service, collapse = ", "),streaming_service)) %>%
+  fill(everything(), .direction = "downup") %>%
+  slice(1)
+
 
 write_csv(masterTitles, "movie_database2.csv")
